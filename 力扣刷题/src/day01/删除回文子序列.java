7af56ab7d6107347699590d8a8e17aeffbcf8856
L-shuai <1472174772@qq.com>
2022-01-22 18:39:03 +0800

    删除回文子序列

---
+++ 力扣刷题/src/day01/删除回文子序列.java
@@ -0,0 +1,27 @@
+package day01;
+
+
+/**
+ *
+ * 给你一个字符串 s，它仅由字母 'a' 和 'b' 组成。每一次删除操作都可以从 s 中删除一个回文 子序列。
+ * 返回删除给定字符串中所有字符（字符串为空）的最小删除次数。
+ * 「子序列」定义：如果一个字符串可以通过删除原字符串某些字符而不改变原字符顺序得到，那么这个字符串就是原字符串的一个子序列。
+ * 「回文」定义：如果一个字符串向后和向前读是一致的，那么这个字符串就是一个回文。
+ *
+ *
+ * 注意：子序列不一定连续
+ */
+public class 删除回文子序列 {
+
+    public static int removePalindromeSub(String s) {
+        boolean flag = true;
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1))
+                return 2;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        System.out.println(removePalindromeSub("ababa"));;
+    }
+}
