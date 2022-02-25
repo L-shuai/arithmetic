2bc20d6f3ce3080e7f1401f8424131830f4a3687
L-shuai <1472174772@qq.com>
2022-02-25 13:08:05 +0800

    二叉树的中序遍历

---
+++ 力扣刷题/src/算法基础/深度优先搜索/二叉树的中序遍历.java
@@ -0,0 +1,26 @@
+package 算法基础.深度优先搜索;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 按照访问左子树——根节点——右子树的方式遍历这棵树，
+ */
+public class 二叉树的中序遍历 {
+
+
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> res = new ArrayList<>();
+//        inorder(root,res);
+//        return res;
+//    }
+//
+//    public void inorder(TreeNode root,List<Integer> res){
+//        if(root==null){
+//            return;
+//        }
+//        inorder(root.left,res);
+//        res.add(root.val);
+//        inorder(root.right,res);
+//    }
+}
