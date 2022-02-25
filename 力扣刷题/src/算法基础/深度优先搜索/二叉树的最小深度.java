aaa670ade25f849ae15d3387621e9a739ef062a1
L-shuai <1472174772@qq.com>
2022-02-25 14:15:58 +0800

    二叉树的最小深度

---
+++ 力扣刷题/src/算法基础/深度优先搜索/二叉树的最小深度.java
@@ -0,0 +1,20 @@
+package 算法基础.深度优先搜索;
+
+public class 二叉树的最小深度 {
+
+//    public int minDepth(TreeNode root) {
+//        if(root==null)
+//            return 0;
+//        if(root.left==null && root.right==null)
+//            return 1;
+//        int min_depth = Integer.MAX_VALUE;
+//        if(root.left!=null){
+//            min_depth = Math.min(minDepth(root.left),min_depth);
+//        }
+//        if(root.right!=null){
+//            min_depth = Math.min(minDepth(root.right),min_depth);
+//        }
+//        min_depth+=1;
+//        return min_depth;
+//    }
+}
