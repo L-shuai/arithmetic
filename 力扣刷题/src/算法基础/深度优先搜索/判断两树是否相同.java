f6cd03e62777b8e3d051d6add6312a79548ce0a4
L-shuai <1472174772@qq.com>
2022-02-25 13:25:29 +0800

    判断两棵树是否相同

---
+++ 力扣刷题/src/算法基础/深度优先搜索/判断两树是否相同.java
@@ -0,0 +1,15 @@
+package 算法基础.深度优先搜索;
+
+public class 判断两树是否相同 {
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if(p==null && q==null)
+//            return true;
+//        else if(p==null || q==null){
+//            return false;
+//        }else if(p.val!=q.val){
+//            return false;
+//        }else{
+//            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+//        }
+//    }
+}
